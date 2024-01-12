9cd8c517743037baea82004b58dc25ee9e798063
troycaselli <swordandtrowel1689@gmail.com>
2024-01-12 11:27:43 -0800

    created search ui

--- GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a85a71ef0a3011bd65bd4e6d58779a21536025d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+597ebc842035a731942230d27ae3ebf8e259fa4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
